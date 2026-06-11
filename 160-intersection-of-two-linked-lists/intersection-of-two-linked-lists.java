0073034b83a835c6555fe184e31127e526b64128
anshbudholiya76 <anshbudholiya76@gmail.com>
2026-06-11 08:54:03 +0530

    Time: 1 ms (99.95%) | Memory: 52.6 MB (77.68%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -1,23 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
 
-        ListNode p1 = headA;
-        ListNode p2 = headB;
+//LENGTH DIFF METHOD
 
-        while (p1 != p2) {
-            if(p1 == null){
-                p1 = headB;
-            }
-            else{
-                p1 = p1.next;
-            }
-            if(p2 == null){
-                p2 = headA;
-            }
-            else{
-                p2 = p2.next;
-            }
-        }
-        return p1;
+    int lenA = 0;
+    int lenB = 0;
+
+    ListNode temp = headA;
+    while(temp != null){
+        lenA++;
+        temp = temp.next;
     }
+
+    temp = headB;
+    while(temp != null){
+        lenB++;
+        temp = temp.next;
+    }
+
+    ListNode p1 = headA;
+    ListNode p2 = headB;
+
+    int diff = Math.abs(lenA - lenB);
+
+    if(lenA > lenB){
+        while(diff-- > 0){
+            p1 = p1.next;
+        }
+    } else {
+        while(diff-- > 0){
+            p2 = p2.next;
+        }
+    }
+
+    while(p1 != p2){
+        p1 = p1.next;
+        p2 = p2.next;
+    }
+
+    return p1;
+}
 }
