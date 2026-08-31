be54136ae868b05fa1be5fb3c71fc48bbcf107cb
anshbudholiya76 <anshbudholiya76@gmail.com>
2026-08-31 23:06:19 +0530

    Time: 2 ms (69.85%) | Memory: 45.5 MB (39.58%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n:nums){
+            set.add(n);
+        }
+        int mul = k;
+        
+        while(set.contains(mul)){
+            mul +=k;
+        }
+        return mul;
+    }
+}
